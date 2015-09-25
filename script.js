99982898f7cc707411f78c3a8e45d60a5db6c061
Chris Gramme <cgramme36@gmail.com>
2015-09-25 01:15:37 -0400

    fs

--- script.js
+++ script.js
@@ -14,81 +14,54 @@ $(document).ready(function(){
 
 function listIn (list, index, interval) {
     if(index < list.length) {
-    $(list[index++]).addClass('fall-in').css({'opacity':'1'}).delay(2000).queue(function(next){
-            $(this).removeClass('fall-in');
-            next();
-        });;
-    setTimeout(function () {
-        listIn(list, index, interval);
+        $(list[index++]).addClass('fall-in').css({'opacity':'1'}).delay(2000).queue(function(next){
+                $(this).removeClass('fall-in');
+                next();
+        });
+        setTimeout(function () {
+            listIn(list, index, interval);
         }, interval);
-      }
     }
+}
 
-var boxSize = 200;
-var divPos = {};
-var parent3d;
-var offset;
-var divXpos;
-var divYpos;
-var scaleSize=1;
+var boxSize=200;
+var scaleSize=1.15;
+var scale3d=30;
 $(document).mousemove(function(e){
+    var divPos={};
+    var offset;
+    var divXpos;
+    var divYpos;
     offset = $(e.target).parent().offset();
     if($(e.target).parent().attr('class') == "parent-3d"){
-
          divPos = {
             left: e.pageX - offset.left,
             top: e.pageY - offset.top
         };
         if(divPos.left<boxSize && divPos.top<boxSize){
-        if(divPos.left>0 && divPos.top>0){
-            if(divPos.top>boxSize/2){
-                divXpos = (divPos.top-boxSize/2)/(boxSize/18);
-                divYpos = -(divPos.left-boxSize/2)/(boxSize/18);
+            if(divPos.left>0 && divPos.top>0){
+                if(divPos.top>boxSize/2){
+                    divXpos = (divPos.top-boxSize/2)/(boxSize/30);
+                    divYpos = -(divPos.left-boxSize/2)/(boxSize/30);
+                }else{
+                    divXpos = -((boxSize/2)-divPos.top)/(boxSize/30);
+                    divYpos = ((boxSize/2)-divPos.left)/(boxSize/30);
+                }
+                $('.child-3d').css({'transform':'rotateY(0deg) rotateX(0deg)'});
+                $(e.target).parents('li').css({'z-index':'10'});
+                $(e.target).css({'transform':'rotateY('+divYpos+'deg) rotateX('+divXpos+'deg) scale('+scaleSize+')','opacity':'1'});
             }else{
-                divXpos = -((boxSize/2)-divPos.top)/(boxSize/18);
-                divYpos = ((boxSize/2)-divPos.left)/(boxSize/18);
+                $('.child-3d').css({'transform':'rotateY(0deg) rotateX(0deg)'});
+                $('li').css({'z-index':'1'});
             }
-            $('h1').html("X = "+divXpos+" Y = "+divYpos);
-            $('.child-3d').css({'transform':'rotateY(0deg) rotateX(0deg)'});
-            $(e.target).parents('li').css({'z-index':'10'});
-            console.log($(e.target).parents('li').attr('class')+"   "+$('li>.parent-3d').attr('class'));
-            //$('.child-3d').addClass('fade-out');
-            $(e.target).removeClass('fade-out').css({'transform':'rotateY('+divYpos+'deg) rotateX('+divXpos+'deg) scale(1.1)','opacity':'1'});
-
-
-
-
-
-
-
         }else{
-            //$('.child-3d').addClass('fade-out');
             $('.child-3d').css({'transform':'rotateY(0deg) rotateX(0deg)'});
             $('li').css({'z-index':'1'});
         }
     }else{
-        //$('.child-3d').addClass('fade-out');
-        //console.log("X = "+divPos.left+" Y = "+divPos.top);
-        $('.child-3d').css({'transform':'rotateY(0deg) rotateX(0deg)'});
-        $('li').css({'z-index':'1'});
-    }
-
-
-
-
-
-    }else{
-        $('.child-3d').removeClass('fade-out');
         offset = 1;
         $('.child-3d').css({'transform':'rotateY(0deg) rotateX(0deg)'});
         $('li').css({'z-index':'1'});
     }
-
-
-
-
-
-    
-    
 });
 
